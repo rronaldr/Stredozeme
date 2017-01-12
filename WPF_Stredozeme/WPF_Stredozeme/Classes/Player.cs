89ee4124990ea785c5cb26c7480d3f0d1fce75b7
Ronald <ronaldrronaldr@gmail.com>
2017-01-12 16:33:55 +0100

    Přidán custom font

--- WPF_Stredozeme/WPF_Stredozeme/Classes/Player.cs
+++ WPF_Stredozeme/WPF_Stredozeme/Classes/Player.cs
@@ -8,5 +8,20 @@ namespace WPF_Stredozeme.Classes
 {
     class Player
     {
+        /// <summary>
+        /// Player class constructor
+        /// </summary>
+        public Player(string name, string role, int level, int attackdmg, int health, int currHealth)
+        {
+            _name = name;
+            _role = role;
+            _level = level;
+            _attackdmg = attackdmg;
+            _health = health;
+            _currHealth = currHealth;
+        }
+
+        private string _name, _role;
+        private int _level, _attackdmg, _health, _currHealth;
     }
 }
